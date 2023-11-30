761a2f738dce228b8fb2a488a59ef670cae3bcbf
Henri <hl.kordt@gmx.de>
2023-11-30 21:52:09 +0100

    wandb

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import torch
 from tqdm import tqdm
+import os
+
 from transformers import AutoTokenizer, AutoModel
 
 from src.data import load_pd, ComplexWordDataset
 from src.model import ModelForTokenRegression, predict_batch
 from src.utils import depict_sample
+
+import wandb
 
 torch.set_num_threads(1)
 
@@ -16,9 +20,13 @@ def main(
     lr=1e-5,
     num_epochs=10,
     binary=False,
+    logging="none"
 ):
     tokenizer = AutoTokenizer.from_pretrained(backbone_name)
-    
+    if logging == "wandb":
+        #weights & biases:
+        wandb.init(project="complex_word_identification")
+
     # model stuff
     backbone = AutoModel.from_pretrained(backbone_name, add_pooling_layer=False)
     if finetune == "adapter":
@@ -68,15 +76,18 @@ def main(
         device,
         num_epochs=num_epochs,
         tokenizer=tokenizer,
-        binary=binary
+        binary=binary,
+        logging=logging
     )
 
     # save model
     # NOTE: this could be nicer... (dont save backbone)
+    if not os.path.exists("./models"):
+        os.makedirs("./models")
     torch.save(model, f"./models/{backbone_name}_{finetune}_{lr}_{num_epochs}_{binary}.pt")
 
 
-def train(model, train_loader, dev_loader, optimizer, scheduler ,device, num_epochs=3, tokenizer=None, binary=True):
+def train(model, train_loader, dev_loader, optimizer, scheduler ,device, num_epochs=3, tokenizer=None, binary=True, logging="none"):
     # Loss function
     if binary:
         criterion = torch.nn.BCELoss()
@@ -88,8 +99,11 @@ def train(model, train_loader, dev_loader, optimizer, scheduler ,device, num_epo
         total_loss = 0
         samples_seen = 0
         # print lr
-        for param_group in optimizer.param_groups:
+        #dictory for logging lr for each param_group
+        lr_log = dict()
+        for group_idx,param_group in enumerate(optimizer.param_groups):
             print("LR:", param_group['lr'])
+            lr_log[f"param_group_{group_idx}_lr"] = param_group['lr']
 
         for batch in (pbar := tqdm(train_loader, desc=f"Training Epoch {epoch + 1}")):
             outputs, label_probs, _ = predict_batch(model, batch, device, binary=binary)
@@ -106,7 +120,9 @@ def train(model, train_loader, dev_loader, optimizer, scheduler ,device, num_epo
             pbar.set_postfix({'loss': total_loss / samples_seen})
 
         print(f"Epoch {epoch + 1}: Average Training Loss = {total_loss / samples_seen}")
+        
 
+        
         # Validation loop
         model.eval()
         total_val_loss = 0
@@ -130,6 +146,13 @@ def train(model, train_loader, dev_loader, optimizer, scheduler ,device, num_epo
         print(f"Epoch {epoch + 1}: Average Validation Loss = {total_val_loss / total_val_seen}")
         print()
 
+        #log val-loss
+        if logging == "wandb":
+            metrics = {"train-loss": total_loss / samples_seen,"val-loss": total_val_loss / total_val_seen, "epoch": epoch+1}
+            metrics.update(lr_log) 
+            print(f"logging: {metrics}" )
+            wandb.log(metrics)
+
         print("Random train sample:")
         batch = next(iter(train_loader))
         outputs, label_probs, token_ids = predict_batch(model, batch, device, binary=binary)
@@ -150,6 +173,7 @@ def parse_args():
     parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--num_epochs", type=int, default=10)
     parser.add_argument("--binary", action="store_true")
+    parser.add_argument("--logging", type=str, default="none")
     return parser.parse_args()
 
 
@@ -163,4 +187,5 @@ if __name__ == "__main__":
         lr=args.lr,
         num_epochs=args.num_epochs,
         binary=args.binary,
+        logging=args.logging
     )
