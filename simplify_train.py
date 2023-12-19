efe2a8d8510f0ce50464e314044495f8cc63803d
Hergen Lehmann <hergen@hergen.eu>
2023-12-19 17:37:28 +0100

    idk

--- simplify_train.py
+++ simplify_train.py
@@ -15,7 +15,7 @@ nltk.download('punkt')
 
 from src.simplify.dataset import ParaphraseDataset, get_chatgpt_dataset_pd
 
-def main(backbone_name, device, batch_size, lr, epochs, cwi_model_path, only_source="cnn_news"):
+def main(backbone_name, device, batch_size, lr, epochs, cwi_model_path, only_source="cnn_news", cwi_multiplier=1.0):
 
     cwi_model = ModelForTokenRegression.load(cwi_model_path).to(device)
 
@@ -51,12 +51,15 @@ def main(backbone_name, device, batch_size, lr, epochs, cwi_model_path, only_sou
     val_loader = DataLoader(val_dataset, batch_size=batch_size)
 
     # Optimizer
-    optimizer = AdamW(model.parameters(), lr=lr)
+    paramerters_to_optimize = [param for param in model.parameters() if param.requires_grad]
+    print(f"Number of parameters to optimize: {sum([param.numel() for param in paramerters_to_optimize])}")
+    print(f"Number of parameters in model: {sum([param.numel() for param in model.parameters()])}")
+    optimizer = AdamW(paramerters_to_optimize, lr=lr)
     
     # Training Loop
     for epoch in range(epochs):
         print(f"Epoch {epoch+1}")
-        train_epoch(model, train_loader, optimizer, cwi_model)
+        train_epoch(model, train_loader, optimizer, cwi_model, cwi_multiplier=cwi_multiplier)
         avg_bleu_score = calculate_bleu_score(model, val_loader, tokenizer, device)
         print(f"Average BLEU Score on Validation Set: {avg_bleu_score}")
 
@@ -92,7 +95,7 @@ def calculate_bleu_score(model, dataloader, tokenizer, device):
     return avg_bleu_score
 
 
-def train_epoch(model, train_loader, optimizer, cwi_model):
+def train_epoch(model, train_loader, optimizer, cwi_model, cwi_multiplier=1.0):
     # Training Loop
     model.train()
     cwi_model.eval()
@@ -112,24 +115,32 @@ def train_epoch(model, train_loader, optimizer, cwi_model):
         cwi_inputs = outputs.logits.argmax(dim=-1)
         # craet attention mask based in special tokens
         cwi_attention_mask = (cwi_inputs != model.config.pad_token_id).to(torch.long)
-        cwi_loss = cwi_model(cwi_inputs, cwi_attention_mask).mean()
+        cwi_logits = cwi_model(cwi_inputs, cwi_attention_mask).squeeze(-1)
+        # camp 0, 1
+        cwi_logits = torch.clamp(cwi_logits, 0, 1)
+        # calculate loss (mae)
+        cwi_loss = (cwi_logits * cwi_attention_mask).mean() * cwi_multiplier
+
+        para_loss = loss.item()
+        cwi_loss = cwi_loss.item()
 
         loss += cwi_loss
 
         loss.backward()
         optimizer.step()
-        pbar.set_description(f"Train Loss: {loss.item()}")
+        pbar.set_description(f"Train Loss: {loss.item():.3f}, Para Loss: {para_loss:.3f}, CWI Loss: {cwi_loss:.3f}")
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--backbone', type=str, default='humarin/chatgpt_paraphraser_on_T5_base')
+    parser.add_argument('--cwi_multiplier', type=float, default=1.0)
     parser.add_argument('--device', type=str, default='mps')
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--lr', type=float, default=5e-5)
     parser.add_argument('--epochs', type=int, default=3)
     parser.add_argument('--cwi_model_path', type=str, default='./models/cwi/humarin/chatgpt_paraphraser_on_T5_base_adapter_0.001_10_False')
-    parser.add_argument('--only_source', type=str, default=None, choices=["cnn_news", "quora", "wikipedia"])
+    parser.add_argument('--only_source', type=str, default=None, choices=["cnn_news", "quora", "squad_2", None])
     args = parser.parse_args()
-    main(args.backbone, args.device, args.batch_size, args.lr, args.epochs, args.cwi_model_path, args.only_source)
+    main(args.backbone, args.device, args.batch_size, args.lr, args.epochs, args.cwi_model_path, args.only_source, args.cwi_multiplier)
